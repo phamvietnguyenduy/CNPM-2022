f1f4881206ca11189c37bb700e57519f41aa89a0
bcdat240601 <78672893+bcdat240601@users.noreply.github.com>
2021-12-08 09:42:06 +0700

    update code

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
+import { BrowserRouter, Routes, Route, useLocation, Link } from "react-router-dom";
 //Components
 import Header from "./Header/Header";
 import Banner from "./Banner/Banner";
@@ -14,6 +14,7 @@ import api from "./api";
 import {useState, useEffect} from 'react';
 import PaginationL from "./PaginationL/PaginationL";
 import Items from "./Items/Items";
+import DetailsInfo from "./DetailsInfo/DetailsInfo";
 //Styles
 
 export default function Shop({ category }) {
@@ -22,22 +23,29 @@ export default function Shop({ category }) {
   const [Posts,setPosts] = useState([]);  
   const search = useLocation().search;
   const id = new URLSearchParams(search).get('id');
+  const idpro = new URLSearchParams(search).get('idpro');
   // lấy dữ liệu từ api khi load hoặc load lại trang
   useEffect(() => {        
     if (window.sessionStorage.getItem("currentPage") === null) {
       window.sessionStorage.setItem("currentPage",1);
     }
-    if (id === null) {
+    if (id === null && idpro === null) {
       api.APIPost("shop?page="+window.sessionStorage.getItem("currentPage")).then(res =>{    
         // console.log(res.data.allproduct);        
         setPosts(res.data.allproduct.data);
         setpageCount(res.data.allproduct.last_page);
       })
-    }else{
+    }if(id !== null){
       api.APIPost("shop/category?id="+id).then(res =>{    
         console.log(res.data.productcate);        
         setPosts(res.data.productcate.data);
         setpageCount(res.data.productcate.last_page);
+      })
+    }if(idpro !== null){
+      api.APIPost("shop/detail?idpro="+idpro).then(res =>{    
+        console.log(res.data.productdetail);        
+        setPosts(res.data.productdetail);
+        setpageCount(0);
       })
     }
     
@@ -46,17 +54,26 @@ export default function Shop({ category }) {
 
   // nhả dữ liệu
   const renderPost = ()=> {     
-    if (Posts !== []) {
+    if (idpro === null) {
      
       return Posts.map((item)=>{
         return (
+         <Link to={"?idpro="+item.product_id}>
           <Items
           name={item.name}
           price={item.price}
           />
+          </Link>
         );
-      }); 
-    }     
+      }
+      ); 
+    }else{
+      return Posts.map((item)=>{
+        return (
+         <DetailsInfo title={item.name} price={item.price}></DetailsInfo>
+        );
+      });
+    }
   }  
 
   // trả về dữ liệu khi đổi trang
@@ -94,7 +111,7 @@ export default function Shop({ category }) {
               pagechange={changePage}
             />}
               
-            
+            {console.log(pageCount)}
             {/* {category === "items1" && (
               <Pagination
                 arrItems={DATA}
